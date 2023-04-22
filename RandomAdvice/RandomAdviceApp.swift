a054be955fc391ef5b0f8c91635cc07315253c36
Justin Hui <jhui@lcs.on.ca>
2023-04-22 16:50:11 -0400

    Initial Commit

---
+++ RandomAdvice/RandomAdviceApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomAdviceApp.swift
+//  RandomAdvice
+//
+//  Created by Justin Hui on 22/4/2023.
+//
+
+import SwiftUI
+
+@main
+struct RandomAdviceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
