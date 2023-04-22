7284b6dd475519ea02b4afd457953a9b3aa440b8
Justin Hui <jhui@lcs.on.ca>
2023-04-22 16:51:25 -0400

    Deleted content view and added a advice view inside a view folder

---
+++ RandomAdvice/Views/AdviceView.swift
@@ -0,0 +1,20 @@
+//
+//  AdviceView.swift
+//  RandomAdvice
+//
+//  Created by Justin Hui on 22/4/2023.
+//
+
+import SwiftUI
+
+struct AdviceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AdviceView_Previews: PreviewProvider {
+    static var previews: some View {
+        AdviceView()
+    }
+}
