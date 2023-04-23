d7cae9aefa590533b540313f7f794d733266f201
Justin Hui <jhui@lcs.on.ca>
2023-04-22 20:38:09 -0400

    Added a favourites view

---
+++ RandomAdvice/Views/FavouritesView.swift
@@ -0,0 +1,20 @@
+//
+//  FavouritesView.swift
+//  RandomAdvice
+//
+//  Created by Justin Hui on 22/4/2023.
+//
+
+import SwiftUI
+
+struct FavouritesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FavouritesView_Previews: PreviewProvider {
+    static var previews: some View {
+        FavouritesView()
+    }
+}
